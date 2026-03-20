4c527a2a95a5b6eadf56b9e28e4a0f672e466d1e
rkbansal <rohitkumarbansal23@gmail.com>
2026-03-20 12:26:05 +0530

    add comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
-    container: {
+    container: { //..
       center: true,
       padding: "2rem",
       screens: {
